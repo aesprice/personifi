1cd7a824debfc195cc8681baa368692a6a6d4d1d
Adam Price <aesprice@gmail.com>
2014-07-07 17:37:57 -0700

    getting mood from server

--- client/avatar/avatar.js
+++ client/avatar/avatar.js
@@ -8,8 +8,28 @@ angular.module('myApp.main.avatar', ['ui.router'])
       controller: 'AvatarController'
     });
 })
-.controller('AvatarController', function ($scope) {
-  $scope.emoteStyle = {
-    backgroundColor: 'rgb(' + Math.floor(255 * (1 - $scope.$parent.mood)) + ',128,' + Math.floor(255 * $scope.$parent.mood) + ')'
+.factory('AvatarFactory', function($http){
+  var methods = {};
+  methods.getServerMood = function(userMood, callback){
+    var data = {userMood: userMood};
+    $http.post('/mood', data)
+      .success(function(data){
+        callback(data);
+      })
+      .error(function(){
+        console.log('no mood from server  :[');
+      });
   };
+  return methods;
+})
+.controller('AvatarController', function ($scope, AvatarFactory) {
+  $scope.getServerMood = AvatarFactory.getServerMood;
+  $scope.serverMood;
+  $scope.emoteStyle = {};
+  $scope.getServerMood($scope.$parent.mood, function(data){
+    $scope.serverMood = data.serverMood;
+    $scope.emoteStyle = {
+      backgroundColor: 'rgb(' + Math.floor(255 * (1 - $scope.serverMood)) + ',128,' + Math.floor(255 * $scope.serverMood) + ')'
+    };
+  });
 });
--- server/main/config.js
+++ server/main/config.js
@@ -19,4 +19,9 @@ module.exports = exports = function (app, express, routers) {
   app.use('/note', routers.NoteRouter);
   app.use(middle.logError);
   app.use(middle.handleError);
+
+  app.post('/mood', function(req, res){
+    var mood = req.body.userMood;
+    res.json({serverMood: mood});
+  });
 };

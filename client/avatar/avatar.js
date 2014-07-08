a0f4a29ade786cbab5c27ca3fcf285e453cbb70c
Adam Price <aesprice@gmail.com>
2014-07-08 15:39:37 -0700

    weighted mood for better color feedback

--- client/avatar/avatar.js
+++ client/avatar/avatar.js
@@ -10,8 +10,7 @@ angular.module('myApp.main.avatar', ['ui.router'])
 })
 .factory('AvatarFactory', function($http){
   var methods = {};
-  methods.getMood = function(userMood, callback){
-    // var data = {userMood: userMood};
+  methods.getMood = function(callback){
     $http.get('/mood')
       .success(function(data){
         callback(data);
@@ -20,17 +19,35 @@ angular.module('myApp.main.avatar', ['ui.router'])
         console.log('no mood from server  :[');
       });
   };
+  methods.weightedMood = function(mood){
+    var weight = 0.80;
+    var wMood;
+    if(mood > 0.6){
+      wMood = mood / weight;
+      if(wMood > 1){
+        wMood = 1;
+      }
+    }else if(mood < 0.4){
+      // invert mood value so we can apply the same math to low moods 
+      wMood = (1-((1-mood) / weight));
+      if(wMood < 0){
+        wMood = 0;
+      }
+    }
+    return wMood || mood;
+  };
   return methods;
 })
 .controller('AvatarController', function ($scope, AvatarFactory) {
   $scope.getMood = AvatarFactory.getMood;
   $scope.serverMood;
   $scope.emoteStyle = {};
-  $scope.getMood($scope.$parent.mood, function(data){
+  $scope.getMood(function(data){
     $scope.serverMood = data.serverMood;
-    console.log($scope.serverMood);
-    var y = Math.floor(191 * $scope.serverMood) + 64; // will be used for red and green, making yellow
-    var b = Math.floor(32 * (1 - $scope.serverMood)) + 128;
+    var displayMood = AvatarFactory.weightedMood($scope.serverMood);
+    console.log(displayMood);
+    var y = Math.floor(191 * displayMood) + 64; // will be used for red and green, making yellow
+    var b = Math.floor(32 * (1 - displayMood)) + 128;
     $scope.emoteStyle = {
       backgroundColor: 'rgb(' + y + ',' + y + ',' + b + ')'
     };
